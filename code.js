03b90e2c4504dd614741943bd091d12779c98e38
Lachlan Shoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2023-12-11 01:04:40 +1100

    🖼 add decorative columns, fix history, add new photos

--- code.js
+++ code.js
@@ -2,6 +2,10 @@ const viewCollectionLink = document.getElementById('viewCollectionLink');
 const homePage = document.getElementById('home');
 const collectionPage = document.getElementById('collection');
 const logo = document.getElementById('logo');
+const leftDecorativeColumn = document.getElementById('leftDecorativeColumn');
+const rightDecorativeColumn = document.getElementById('rightDecorativeColumn');
+
+let page = '';
 
 /**
  * Displays the collection page and hides the home page.
@@ -14,14 +18,19 @@ const showCollection = () => {
 const showHome = () => {
   homePage.style.display = 'flex';
   collectionPage.style.display = 'none';
-  history.pushState('', document.title, window.location.pathname);
 };
 
 const checkHash = () => {
   if (window.location.hash === '#collection') {
     showCollection();
+    page = 'collection';
+    viewCollectionLink.innerText = 'View pieces';
+    viewCollectionLink.setAttribute('href', '#');
   } else {
     showHome();
+    page = 'home';
+    viewCollectionLink.innerText = 'View collection';
+    viewCollectionLink.setAttribute('href', '#collection');
   }
 };
 
@@ -38,3 +47,55 @@ window.addEventListener('popstate', () => {
 logo.addEventListener('click', () => {
   showHome();
 });
+
+/**
+ * Creates a new element of the specified type and appends it to the specified parent element.
+ *
+ * @param {string} element - The type of element to create.
+ * @param {HTMLElement} parent - The parent element to append the new element to.
+ * @param {object} attributes - An object containing the attributes to set on the new element.
+ */
+const createAndAppend = (element, parent, attributes) => {
+  const newElement = document.createElement(element);
+  parent.appendChild(newElement);
+  for (const attribute in attributes) {
+    newElement.setAttribute(attribute, attributes[attribute]);
+  }
+};
+
+const images = [
+  'assets/flat/IMG_0952.webp',
+  'assets/flat/IMG_0954.webp',
+  'assets/flat/IMG_0962.webp',
+  'assets/flat/IMG_0965.webp',
+  'assets/flat/IMG_0968.webp',
+  'assets/flat/IMG_0970.webp',
+];
+
+for (let i = 0; i < 100; i++) {
+  const imageIndex = Math.floor(Math.random() * 6);
+  const image = images[imageIndex];
+  const leftRange = Math.floor(Math.random() * 20 - 5);
+  const rightRange = Math.floor(Math.random() * (74 - 20) + 75);
+
+  let leftPercentage;
+  let topPercentage = Math.floor(Math.random() * (100 + 5) - 5);
+  let decorativeColumn;
+
+  // determine position
+  const position = Math.floor(Math.random() * 2);
+  if (position === 0) {
+    leftPercentage = leftRange;
+    decorativeColumn = leftDecorativeColumn;
+  } else if (position === 1) {
+    leftPercentage = rightRange;
+    decorativeColumn = rightDecorativeColumn;
+  }
+
+  createAndAppend('img', decorativeColumn, {
+    class: 'sideHomeImage floating',
+    src: image,
+    'aria-hidden': true,
+    style: `left: ${leftPercentage}%; top: ${topPercentage}%;`,
+  });
+}
